2506885f9f9d196b8021c840001fc9a48cb1ae68
Marta <martadipuccio@me.com>
2018-05-23 11:43:51 +0200

    filter from events.json, id route and home with aqm filter

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-   const express = require('express');
-   const app = express()
-   //const dotenv = require('dotenv')
-   require('dotenv').config()
+    const express = require('express');
+    const app = express()
    
+    require('dotenv').config()
 
-   app.get('/', (req, res) => res.send('home'))
-   app.get('/:id', (req, res) => {
-       res.send('id route Marta')
-   })
-   app.listen(process.env.PORT, () => console.log('App listening on port 5000'))
+    var fs = require('fs');
+    var file = fs.readFileSync('./events.json');
+    
+    var obj = JSON.parse(file);
+    
+    app.get('/:id', (req, res) => {
+        console.log(req.params) 
 
-   console.log('heeelelelelellel')
+        res.send(obj.filter( item => item._id["$oid"] == req.params.id))
+    })
+    
+    
+    //app.get('/', (req, res) => res.send('home'))
+    app.get('/', (req, res) => {
+        console.log(req.query.aqm)
+        res.send(obj.filter(o => o.aqm.r3000["$numberInt"] > 70))
+    })
+    
+    app.listen(process.env.PORT, () => console.log('App listening on port 5000'))
+
+
+    //req.params contains matched route parameters only.
+

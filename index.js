a4fd372f06cafaa4521b863ce71d63dd2dd21782
Marta <martadipuccio@me.com>
2018-05-23 13:49:37 +0200

    created a third route(events),which  returns the whole event, then home will sends back a simple html file, and append some results(ids) from the json

--- index.js
+++ index.js
@@ -7,20 +7,24 @@
     var file = fs.readFileSync('./events.json');
     
     var obj = JSON.parse(file);
+
+    //app.get('/', (req, res) => res.send('home'))
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/index.html');
+    })
     
-    app.get('/:id', (req, res) => {
+    app.get('/events', (req, res) => {
+        console.log(req.query.aqm)
+        //res.send(obj.filter(o => o.aqm.r3000["$numberInt"] > 70))
+        res.send(obj)
+    })
+
+    app.get('/events/:id', (req, res) => {
         console.log(req.params) 
 
         res.send(obj.filter( item => item._id["$oid"] == req.params.id))
     })
-    
-    
-    //app.get('/', (req, res) => res.send('home'))
-    app.get('/', (req, res) => {
-        console.log(req.query.aqm)
-        res.send(obj.filter(o => o.aqm.r3000["$numberInt"] > 70))
-    })
-    
+
     app.listen(process.env.PORT, () => console.log('App listening on port 5000'))
 
 

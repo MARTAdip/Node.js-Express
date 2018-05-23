7202ce698573e657ac4ce289c7fdc2fd19b975b8
Marta <martadipuccio@me.com>
2018-05-23 15:07:15 +0200

    added a random route, to generate random obj from the json event, and a route if a route is not found

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
+    require('dotenv').config()
     const express = require('express');
     const app = express()
-   
-    require('dotenv').config()
 
     var fs = require('fs');
     var file = fs.readFileSync('./events.json');
-    
     var obj = JSON.parse(file);
 
+    
     //app.get('/', (req, res) => res.send('home'))
     app.get('/', (req, res) => {
         res.sendFile(__dirname + '/index.html');
     })
-    
-    app.get('/events', (req, res) => {
+    app.get('/api/events', (req, res) => {
         console.log(req.query.aqm)
         //res.send(obj.filter(o => o.aqm.r3000["$numberInt"] > 70))
         res.send(obj)
     })
-
-    app.get('/events/:id', (req, res) => {
+    app.get('/api/events/random', (req, res) => {
+        console.log("route /api/events/random")
+        res.send(obj[Math.floor(Math.random() * (obj.length - 0 +1)) +0])
+    })
+    app.get('/api/events/:id', (req, res) => {
         console.log(req.params) 
-
         res.send(obj.filter( item => item._id["$oid"] == req.params.id))
+    })
+    app.get('*', (req, res) => {
+        console.log("route/ fallback")
+        res.send("route not found") //  handling route that does not exist
     })
 
     app.listen(process.env.PORT, () => console.log('App listening on port 5000'))
